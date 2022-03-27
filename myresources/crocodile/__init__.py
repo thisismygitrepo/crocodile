e31876462a6df5030292223c39f7abe2cfb0db4c
Alex Al-Saffar <programmer@usa.com>
2022-03-27 22:51:36 +1030

    new version

--- myresources/crocodile/__init__.py
+++ myresources/crocodile/__init__.py
@@ -1,9 +1,9 @@
 
-__version__ = "5.8"
+__version__ = "5.91"
 __author__ = "Alex Al-Saffar"
 __date__ = "2022-03-16"
 docs = r"https://crocodile.readthedocs.io/en/latest/"
 git = r"https://github.com/thisismygitrepo/crocodile"
 release_notes = """
-Bug fixes
+concice version.
 """
