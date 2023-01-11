43173c4dcbd7bbf9c369c1fcc494d3125aedb036
Alex Al-Saffar <programmer@usa.com>
2023-01-12 07:26:17 +1030

    new release

--- myresources/crocodile/__init__.py
+++ myresources/crocodile/__init__.py
@@ -1,12 +1,12 @@
 
-__version__ = "8.20"
+__version__ = "8.40"
 __author__ = "Alex Al-Saffar"
-__date__ = "2022-11-01"
+__date__ = "2023-01-12"
 docs = r"https://crocodile.readthedocs.io/en/latest/"
 git = r"https://github.com/thisismygitrepo/crocodile"
 pypi = r"https://pypi.org/project/crocodile"
 release_notes = """
-Minor fixes
+Added Cluster for distributed computing.
 """
 
 # github workflow, push latest changes, make new tag, push tag, and github action will automatically push new release to pypi.
