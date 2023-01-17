d86d14782f2d29afa39f8ab2a1ada32230e5a6cb
Alex Al-Saffar <programmer@usa.com>
2023-01-17 20:34:02 +1030

    fixed res_dir

--- myresources/crocodile/cluster/trial_file.py
+++ myresources/crocodile/cluster/trial_file.py
@@ -16,7 +16,7 @@ def expensive_function() -> tb.P:
     print("I'm done, I crunched a lot of numbers. Next I'll save my results to a file and passing its directory back to the main process on the machine running me.")
     path = tb.P.tmpdir().joinpath("result.Struct.pkl")
     tb.S(a=a).save(path=path)
-    return path.parent
+    return path.parent  # extra job details files will be added to this res_folder
 
 
 # parallelizeBegins
@@ -31,25 +31,27 @@ def parallelize(idx_start: int, idx_end: int, idx_max: int, num_instances: int) 
     kwargs_split = tb.L(range(idx_start, idx_end, 1)).split(to=num_instances).apply(lambda sub_list: dict(idx_start=sub_list[0], idx_end=sub_list[-1], idx_max=idx_max))
     for idx, x in enumerate(kwargs_split):
         tb.S(x).print(as_config=True, title=f"Instance {idx}")
+    print("\n" * 2)
 
-    res = kwargs_split.apply(lambda kwargs: expensive_function_single_thread(**kwargs), jobs=num_instances)
+    save_dir_suffix = f"machine_{idx_start}_{idx_end}"
+
+    res = kwargs_split.apply(lambda kwargs: expensive_function_single_thread(**kwargs, save_dir_suffix=save_dir_suffix), jobs=num_instances)
     return tb.P(res[0]).parent
 
 
 # parallelizeEnds
 
 
-def expensive_function_single_thread(idx_start, idx_end, idx_max):
+def expensive_function_single_thread(idx_start, idx_end, idx_max, save_dir_suffix):
     print(f"Hello, I am one thread of an expensive function, and I just started running ...")
     execution_time_in_seconds = 60 * 1
     steps = 100
     for _ in track(range(steps), description="Progress bar ..."):
         time.sleep(execution_time_in_seconds/steps)  # Simulate work being done
     print("I'm done, I crunched numbers from {} to {}.".format(idx_start, idx_end))
-    res_dir = tb.P.tmp().joinpath(f"tmp_dirs/expensive_function_single_thread")
-    res_dir.delete(sure=True)
-    tb.S(a=1).save(path=res_dir.joinpath(f"thread_{idx_start}_{idx_end}/trial_func_result.Struct.pkl"))
-    return res_dir
+    save_dir = tb.P.tmp().joinpath(f"tmp_dirs/expensive_function_single_thread").joinpath(save_dir_suffix, f"thread_{idx_start}_{idx_end}").create()
+    tb.S(a=1).save(path=save_dir.joinpath(f"trial_func_result.Struct.pkl"))
+    return save_dir
 
 
 if __name__ == '__main__':
