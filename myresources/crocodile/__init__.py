a63ee62931157e5a676e09187ee59ed40c2d8914
Alex Al-Saffar <programmer@usa.com>
2023-02-24 20:48:02 +1030

    a new release

--- myresources/crocodile/__init__.py
+++ myresources/crocodile/__init__.py
@@ -1,11 +1,11 @@
 
-__version__ = "10.3.5"
+__version__ = "10.4.0"
 __author__ = "Alex Al-Saffar"
-__date__ = "2023-01-13"
+__date__ = "2023-02-24"
 docs = r"https://crocodile.readthedocs.io/en/latest/"
 git = r"https://github.com/thisismygitrepo/crocodile"
 pypi = r"https://pypi.org/project/crocodile"
 release_notes = """
-added ascci art for windows
+added bzip2 compression
 """
 
