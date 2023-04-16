4374bbd24a6e71fd0d5bff375d4500a246461c18
Alex Al-Saffar <alex.al-saffar@sa.gov.au>
2023-04-17 07:45:45 +0930

    fix(plotly_management.py): change host from 0.0.0.0 to localhost to prevent security issues.

--- myresources/crocodile/plotly_management.py
+++ myresources/crocodile/plotly_management.py
@@ -47,7 +47,7 @@ CONFIG = SimpleNamespace(displayModeBar=True,  # always visible.
 class App:
     @staticmethod
     def run(app, debug=False, random_port=True):
-        host = "0.0.0.0"  #  "127.0.0.1"
+        host = "localhost"  # 0.0.0.0"  #  "127.0.0.1"
         port = tb.randstr(lower=False, upper=False, length=4) if random_port else "8050"  # Random ports prevent multile programs from crashing into each other.
         # pynoinspection HTTP
         try: tb.P(rf'http://{host}:{port}/')()
