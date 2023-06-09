dbecaea49acbb5cb1de9498a74cd925f3c6cd008
Alex Al-Saffar <programmer@usa.com>
2023-06-09 09:52:13 +0930

    new minor release

--- myresources/crocodile/__init__.py
+++ myresources/crocodile/__init__.py
@@ -1,11 +1,11 @@
 
-__version__ = "11.1.0"
+__version__ = "11.2"
 __author__ = "Alex Al-Saffar"
-__date__ = "2023-03-17"
+__date__ = "2023-06-09"
 docs = r"https://crocodile.readthedocs.io/en/latest/"
 git = r"https://github.com/thisismygitrepo/crocodile"
 pypi = r"https://pypi.org/project/crocodile"
 release_notes = """
-cleaned meta.Scheduler
+cleaned run script
 """
 
