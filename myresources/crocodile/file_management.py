8cb4df842a455583c7666feb8bffecd871de3d3e
Alex Al-Saffar <alex.al-saffar@sa.gov.au>
2023-07-22 19:39:06 +0930

    fix(file_management.py): fix typo in _get_remote_path method parameter feat(file_management.py): add root parameter to _get_remote_path method fix(file_management.py): fix typo in to_cloud method parameter feat(file_management.py): add root parameter to to_cloud method fix(file_management.py): fix typo in from_cloud method parameter feat(file_management.py): add root parameter to from_cloud method fix(file_management.py): fix typo in sync_to_cloud method parameter feat(file_management.py): add root parameter to sync_to_cloud method

--- myresources/crocodile/file_management.py
+++ myresources/crocodile/file_management.py
@@ -337,21 +337,21 @@ class P(type(Path()), Path):
     def share_on_cloud(self) -> 'P': return P(__import__("requests").put(f"https://transfer.sh/{self.expanduser().name}", self.expanduser().absolute().read_bytes()).text)
     def share_on_network(self, username=None, password=None): from crocodile.meta import Terminal; Terminal(stdout=None).run(f"sharing {self} {('--username ' + username) if username else ''} {('--password ' + password) if password else ''}", shell="powershell")
     def to_qr(self, txt=True, path=None): qrcode = install_n_import("qrcode"); qr = qrcode.QRCode(); qr.add_data(str(self) if "http" in str(self) else (self.read_text() if txt else self.read_bytes())); import io; f = io.StringIO(); qr.print_ascii(out=f); f.seek(0); print(f.read()); qr.make_image().save(path) if path is not None else None
-    def _get_remote_path(self, os_specific=False): return P("myhome") / (__import__('platform').system().lower() if os_specific else 'generic_os') / self.rel2home()
-    def to_cloud(self, cloud, remotepath=None, zip=False, encrypt=False, key=None, pwd=None, rel2home=False, share=False, verbose=True, os_specific=False, transfers=10) -> 'P':
+    def _get_remote_path(self, root, os_specific=False): return P(root) / (__import__('platform').system().lower() if os_specific else 'generic_os') / self.rel2home()
+    def to_cloud(self, cloud, remotepath=None, zip=False, encrypt=False, key=None, pwd=None, rel2home=False, share=False, verbose=True, os_specific=False, transfers=10, root="myhome") -> 'P':
         localpath, to_del = self.expanduser().absolute(), []
         if zip: localpath = localpath.zip(inplace=False); to_del.append(localpath)
         if encrypt: localpath = localpath.encrypt(key=key, pwd=pwd, inplace=False); to_del.append(localpath)
-        remotepath = (localpath._get_remote_path(os_specific=os_specific) if rel2home else localpath) if remotepath is None else P(remotepath)
+        remotepath = (localpath._get_remote_path(root==root, os_specific=os_specific) if rel2home else localpath) if remotepath is None else P(remotepath)
         from crocodile.meta import Terminal; print(f" {'>'*10} UPLOADING ⬆️ {localpath.as_posix()} to {cloud}:{remotepath.as_posix()}")
         res = Terminal(stdout=None).run(f"""rclone copyto '{localpath.as_posix()}' '{cloud}:{remotepath.as_posix()}' {'--progress' if verbose else ''} --transfers={transfers}""", shell="powershell").capture(); [item.delete(sure=True) for item in to_del]; print(f" {'>'*10} UPLOAD COMPLETED.")
         assert res.is_successful(strict_err=True, strict_returcode=True), res.print(capture=False)
         if share: print("SHARING FILE"); res = Terminal().run(f"""rclone link '{cloud}:{remotepath.as_posix()}'""", shell="powershell").capture(); return res.op2path(strict_err=True, strict_returncode=True)
         return self
-    def from_cloud(self, cloud, localpath=None, decrypt=False, unzip=False, key=None, pwd=None, rel2home=False, overwrite=True, merge=False, os_specific=False, transfers=10):
+    def from_cloud(self, cloud, localpath=None, decrypt=False, unzip=False, key=None, pwd=None, rel2home=False, overwrite=True, merge=False, os_specific=False, transfers=10, root="myhome"):
         remotepath = self  # .expanduser().absolute()
         localpath = P(localpath).expanduser().absolute() if localpath is not None else P.home().joinpath(remotepath.rel2home())
-        if rel2home: remotepath = remotepath._get_remote_path(os_specific=os_specific)
+        if rel2home: remotepath = remotepath._get_remote_path(root=root, os_specific=os_specific)
         remotepath += ".zip" if unzip else ""; remotepath += ".enc" if decrypt else ""; localpath += ".zip" if unzip else ""; localpath += ".enc" if decrypt else ""
         from crocodile.meta import Terminal; print(f"DOWNLOADING ⬇️ {cloud}:{remotepath.as_posix()} ==> {localpath.as_posix()}")
         res = Terminal(stdout=None).run(f"""rclone copyto '{cloud}:{remotepath.as_posix()}' '{localpath.as_posix()}' --progress --transfers={transfers}""", shell="powershell")
@@ -359,8 +359,8 @@ class P(type(Path()), Path):
         if decrypt: localpath = localpath.decrypt(key=key, pwd=pwd, inplace=True)
         if unzip: localpath = localpath.unzip(inplace=True, verbose=True, overwrite=overwrite, content=True, merge=merge)
         return localpath
-    def sync_to_cloud(self, cloud, sync_up=False, sync_down=False, os_specific=False, rel2home=True, transfers=10, delete=False):
-        source, target = (self.expanduser().absolute().as_posix(), f"{cloud}:{self._get_remote_path(os_specific=os_specific).as_posix() if rel2home else self.expanduser().absolute().as_posix()}") if sync_up else (f"{cloud}:{self._get_remote_path(os_specific=os_specific).as_posix() if rel2home else self.expanduser().absolute().as_posix()}", self.expanduser().absolute().as_posix())
+    def sync_to_cloud(self, cloud, sync_up=False, sync_down=False, os_specific=False, rel2home=True, transfers=10, delete=False, root="myhome"):
+        source, target = (self.expanduser().absolute().as_posix(), f"{cloud}:{self._get_remote_path(root=root, os_specific=os_specific).as_posix() if rel2home else self.expanduser().absolute().as_posix()}") if sync_up else (f"{cloud}:{self._get_remote_path(root=root, os_specific=os_specific).as_posix() if rel2home else self.expanduser().absolute().as_posix()}", self.expanduser().absolute().as_posix())
         if not sync_down and not sync_up: print(f"SYNCING 🔄️ {source} {'<>' * 7} {target}`"); rclone_cmd = f"""rclone bisync '{source}' '{target}' --resync --remove-empty-dirs """
         else: print(f"SYNCING {source} {'>' * 15} {target}`"); rclone_cmd = f"""rclone sync '{source}' '{target}' """
         rclone_cmd += f" --progress --transfers={transfers} --verbose --vfs-cache-mode full"; rclone_cmd += " --delete-during" if delete else ""; from crocodile.meta import Terminal; print(rclone_cmd); res = Terminal(stdout=None).run(rclone_cmd, shell="powershell")
