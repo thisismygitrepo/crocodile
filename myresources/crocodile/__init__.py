0fc48f8c528f37cee2b41ea23b3595167362f78f
Alex Al-Saffar <programmer@usa.com>
2023-08-14 11:50:09 +0930

    new release

--- myresources/crocodile/__init__.py
+++ myresources/crocodile/__init__.py
@@ -1,11 +1,13 @@
 
-__version__ = "11.2"
+"""Info
+"""
+
+__version__ = "12.0"
 __author__ = "Alex Al-Saffar"
-__date__ = "2023-06-09"
+__date__ = "2023-08-14"
 docs = r"https://crocodile.readthedocs.io/en/latest/"
 git = r"https://github.com/thisismygitrepo/crocodile"
 pypi = r"https://pypi.org/project/crocodile"
 release_notes = """
-cleaned run script
+Support for type hinting is added.
 """
-
