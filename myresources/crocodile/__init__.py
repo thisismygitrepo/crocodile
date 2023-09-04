535c61c85a7bf7a16dfa7937d4bcda5d2ee8593e
Alex Al-Saffar <programmer@usa.com>
2023-09-04 11:46:59 +0930

    new version

--- myresources/crocodile/__init__.py
+++ myresources/crocodile/__init__.py
@@ -2,12 +2,12 @@
 """Info
 """
 
-__version__ = "12.0"
+__version__ = "13.0"
 __author__ = "Alex Al-Saffar"
-__date__ = "2023-08-14"
+__date__ = "2023-09-04"
 docs = r"https://crocodile.readthedocs.io/en/latest/"
 git = r"https://github.com/thisismygitrepo/crocodile"
 pypi = r"https://pypi.org/project/crocodile"
 release_notes = """
-Support for type hinting is added.
+Added type hinting to all modules
 """
