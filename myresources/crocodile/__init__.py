f446b257866b4712171c765d80254363e489b059
Alex Al-Saffar <programmer@usa.com>
2023-09-19 09:52:40 +0930

    minor release

--- myresources/crocodile/__init__.py
+++ myresources/crocodile/__init__.py
@@ -2,12 +2,12 @@
 """Info
 """
 
-__version__ = "13.0"
+__version__ = "13.1"
 __author__ = "Alex Al-Saffar"
-__date__ = "2023-09-04"
+__date__ = "2023-09-19"
 docs = r"https://crocodile.readthedocs.io/en/latest/"
 git = r"https://github.com/thisismygitrepo/crocodile"
 pypi = r"https://pypi.org/project/crocodile"
 release_notes = """
-Added type hinting to all modules
+repr enhancements for deep learning data reader.
 """
