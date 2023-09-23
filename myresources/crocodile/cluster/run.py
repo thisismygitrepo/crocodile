fdae4856e12c1eb450bac357784292c5f34bf4e3
Alex Al-Saffar <programmer@usa.com>
2023-09-23 19:41:40 +0930

    fix(run.py): change kill_on_completion flag from False to True to ensure that the cluster is terminated after completion
    The change was made to improve the behavior of the script when running on a cloud environment. By setting `kill_on_completion` to True, the cluster will be terminated automatically after the script finishes executing. This helps to prevent unnecessary resource usage and ensures that the cluster is not left running indefinitely.

--- myresources/crocodile/cluster/run.py
+++ myresources/crocodile/cluster/run.py
@@ -20,7 +20,7 @@ def run_on_cloud(func: Any, split: int, reset_cloud: bool = False, reset_local: 
         copy_repo=False, update_repo=True, install_repo=False, update_essential_repos=True, data=[], transfer_method="cloud", cloud_name="oduq1",
         # remote machine behaviour
         open_console=True, notify_upon_completion=True, to_email=None, email_config_name=None,
-        kill_on_completion=False,
+        kill_on_completion=True,
         launch_method="cloud_manager",
         # execution behaviour
         ipython=False, interactive=False, pdb=False, pudb=False, wrap_in_try_except=True,
