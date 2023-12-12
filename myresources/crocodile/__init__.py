74a46462beb9d94addf80ac49b6a42981d5962df
Alex Al-Saffar <programmer@usa.com>
2023-12-12 16:34:05 +1030

    new release

--- myresources/crocodile/__init__.py
+++ myresources/crocodile/__init__.py
@@ -2,13 +2,12 @@
 """Info
 """
 
-__version__ = "13.5"
+__version__ = "13.6"
 __author__ = "Alex Al-Saffar"
 __date__ = "2023-10-23"
 docs = r"https://crocodile.readthedocs.io/en/latest/"
 git = r"https://github.com/thisismygitrepo/crocodile"
 pypi = r"https://pypi.org/project/crocodile"
 release_notes = """
-Minor enchacements on the print statements
-
+write_text now creates the file if it doesn't exist.
 """
