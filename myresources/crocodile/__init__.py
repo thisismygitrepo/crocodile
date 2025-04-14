153189128cdaae4d7fb48d8cf6fe1d62eca07294
Alex Al-Saffar <programmer@usa.com>
2025-04-14 14:13:04 +0930

    Remove author, date, version, and documentation links from __init__.py

--- myresources/crocodile/__init__.py
+++ myresources/crocodile/__init__.py
@@ -1,12 +1,1 @@
 
-__author__ = "Alex Al-Saffar"
-__date__ = "2025-04-17"
-__version__ = "14.2.2"
-docs = r"https://crocodile.readthedocs.io/en/latest/"
-git = r"https://github.com/thisismygitrepo/crocodile"
-pypi = r"https://pypi.org/project/crocodile"
-release_notes = """
-
-Minor changes
-
-"""
